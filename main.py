e05fa9e2bf966f6f2581bba6c9a16a36c554ff76
try-except-try-accept <chall168421@gmail.com>
2021-03-27 15:26:42 +0700

    dealing and view cards implemented

--- main.py
+++ main.py
@@ -2,10 +2,45 @@ from random import randint, shuffle
 
 SUITS = "SHDC"
 
-MENU = """1. Play a points card.
-2. Play an effect card.
-3. View your hand.
-4. Draw from the deck."""
+MENU = """1. View your hand.
+2. Play a points card.
+3. Play an effect card.
+4. Scuttle an opponent's card.
+5. Draw from the deck."""
+
+class Card:
+    def __init__(self, code):
+        self.code = code
+        self.rank = int(code[0]) if code[0].isdigit() else 0
+        self.points_possible = False
+        if self.rank:
+            self.points_possible = True
+        self.suit = SUITS.index(code[1]) if code[1] in SUITS else None
+
+    def __gt__(self, other):
+
+        if self.rank > other.rank:
+            return True
+        elif self.rank < other.rank:
+            return False
+        else:
+            if self.suit > other.suit:
+                return True
+            return False
+
+    def __repr__(self):
+        return self.get_name()
+
+    def get_name(self):
+        if self.code == "JR":
+            return "The Red Joker!"
+        elif self.code == "JB":
+            return "The Black Joker!"
+        r, s = self.code
+        suits = {"C": "Clubs", "S": "Spades", "D": "Diamonds", "H": "Hearts"}
+        ranks = {"1": "Ace", "J": "Jack", "Q": "Queen", "K": "King"}
+        r = ranks[r] if r in ranks else r
+        return r + " of " + suits[s]
 
 
 class Player:
@@ -25,26 +60,17 @@ class Player:
     def check_win(self, to_win):
         return sum(int(c[0] for c in self.points)) >= to_win
 
+    def has_goggles(self):
+        return any([card[0] == "8" for card in self.perms])
 
 
 
 
 def create_deck():
-    values = "".join(range(1, 11)) + "JQK"
-    return sum([[r+s for s in SUITS] for r in values], []) + ["JR", "JB"]
-
-def get_card_name(card):
-    if card == "JR":    return "The Red Joker!"
-    elif card == "JB":    return "The Black Joker!"
-    r, s = card
-    suits = {"C":"Clubs", "S":"Spades", "D":"Diamonds", "H":"Hearts"}
-    ranks = {"1":"Ace", "J":"Jack", "Q":"Queen", "K":"King"}
-    return ranks[r] if r in ranks else r + " of " + suits[s]
-
-def display_hand(player, perms):
-
-
-    print()
+    values = "".join(map(str, range(1, 11))) + "JQK"
+    # flatten 2d list to 1d with sum(), add jokers and instantiate card objects
+    all_codes = sum([[r+s for s in SUITS] for r in values], []) + ["JR", "JB"]
+    return [Card(code) for code in all_codes]
 
 def get_player_names():
     names = []
@@ -55,35 +81,50 @@ def get_player_names():
             names.append(n)
     return names
 
-def deal_hands(deck, dealer):
+def deal_hands(names, deck, dealer):
     print()
     p1, p2 = [], []
     for i in range(6):
         if i < 6 or dealer:
-            p1.append(deck.pop(0))
-            print("Player 1 drew a ", get_card_name(p1[-1]))
+            card = deck.pop(0)
+            p1.append(card)
+            print(f"{names[0]} drew a {card.get_name()}")
         if i < 6 or dealer:
-            p2.append(deck.pop(0))
-            print("Player 1 drew a ", get_card_name(p2[-1]))
+            card = deck.pop(0)
+            p2.append(card)
+            print(f"{names[1]} drew a {card.get_name()}")
     print()
     return p1, p2
 
 def display_card(cards, index):
-    try:
-        print("{:<30}".format(cards[index], end=""))
-    except IndexError:
-        
+    if not len(cards) or index >= len(cards):
+        return
+
+    print("{:<30}".format(str(cards[index]), end=""))
+
+
 
 
 def display_hands(current_player, other_player):
-    p = current_player
-    cards_to_show = [p.hand, p.points, p.perms]
-    print("{:<30}{:<30}{:<30}".format("HAND", "POINTS CARDS:", "PERMANENT EFFECTS:"))
-    biggest_list = max(cards_to_show, key=len)
-    for i in range(biggest_list):
-        display_card(p.hand, i)
-        display_card(p.points, i)
-        display_card(p.perms, i)
+
+    for i in range(2):
+        p = [current_player, other_player][i]
+        print()
+        print(f"{p.name}'s cards:")
+        print()
+        cards_to_show = [p.hand, p.points, p.perms]
+        print("{:<30}{:<30}{:<30}".format("HAND", "POINTS CARDS:", "PERMANENT EFFECTS:"))
+
+        biggest_list = len(max(cards_to_show, key=len))
+        for j in range(biggest_list):
+            if i == 0 or current_player.has_goggles():
+                display_card(p.hand, j)
+            else:
+                display_card(["?"], 0)
+
+            display_card(p.points, j)
+            display_card(p.perms, j)
+
 
 
 
@@ -92,25 +133,43 @@ def display_hands(current_player, other_player):
 
 
 def validate_choice(current_player, other_player, choice):
-    if choice == "4":
+    if choice == "5":
         return True
-    elif choice == "3":
-        display_hands([current_player, other_player])
+    elif choice == "1":
+        display_hands(current_player, other_player)
     elif not len(current_player.hand):
         print("You have no cards left!")
-    elif choice == "1":
-        return any([card[0].isdigit() for card in current_player.hand]) # any card with a digit rank is points.
-    elif choice == "2":
+    elif choice == "3":
         return True
+    elif choice in ["2", "4"]:
+        your_points = [card.rank for card in current_player.hand if card.points_possible]
+        their_points = [card.rank for card in other_player.hand if card.points_possible]
+        if not len(your_points):
+            print("You don't have any cards that can be played as points cards.")
+            return False
+        else:
+            if choice == "2":
+                return True
+            else:
+                if any([your_card > other_card for your_card, other_card in zip(sorted(current_player.hand), sorted(other_player.points))]):
+                    return True
+                else:
+                    print("You don't have any cards in your hand higher in value than any of your opponent's points cards.")
+                    return False
+
     else:
         print("I don't understand that.")
 
     return False
 
+def validate_card(other_player, choice):
+    return True
+
+
 def select_card(hand, choice):
     valid = list(map(str, range(1, len(hand))))
     for i, card in enumerate(hand):
-        print("Enter 1 to choose the", get_card_name(card))
+        print("Enter 1 to choose the", card.get_name())
     choice = input()
     while choice not in valid:
         choice = input("I didn't understand that.")
@@ -118,7 +177,6 @@ def select_card(hand, choice):
     chosen_card = hand.pop(int(choice)-1)
 
     return chosen_card, hand
-
 
 def game():
 
@@ -141,12 +199,12 @@ def game():
         print("Player 2 will be the dealer.")
         dealer = 1
 
-    p1_hand, p2_hand = deal_hands(deck, dealer)
+    p1_hand, p2_hand = deal_hands(names, deck, dealer)
 
     player1 = Player(names[0], p1_hand)
-    player2 = Player(names[0], p2_hand)
+    player2 = Player(names[1], p2_hand)
 
-    while p1_win and not p2_win:
+    while not p1_win and not p2_win:
 
         if turn % 2 == 0:
             current_player, other_player = player1, player2
@@ -161,9 +219,10 @@ def game():
             print(MENU)
 
             choice = input()
-            if validate_choice(current_player, other_player):
-                card, hand = select_card(current_player, other_player, choice)
-                p1_points, p2_points, p1_perms, p2_perms = process_move(card, choice, perms)
+            if validate_choice(current_player, other_player, choice):
+                card, hand = select_card(current_player, choice)
+                if validate_card(other_player, choice):
+                    process_move(card, current_player, other_player, choice)
                 move_complete = True
 
 
@@ -177,3 +236,5 @@ def game():
 
 
 
+if __name__ == "__main__":
+    game()
